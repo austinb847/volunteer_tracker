8f85ec94a88642ed4ecd42b06f79c7382b01f881
austinb847 <austinb847@icloud.com>
2020-05-22 18:41:36 -0700

    create routes to edit volunteer, delete volunteer

--- app.rb
+++ app.rb
@@ -24,19 +24,55 @@ post('/projects') do
 end
 
 get('/projects/:id/edit') do
-  @project = Project.find(params[:id])
+  @project = Project.find(params[:id].to_i)
   erb(:edit_project)
 end
 
 patch('/projects/:id') do
   title = params[:project_title]
-  project = Project.find(params[:id])
+  project = Project.find(params[:id].to_i)
   project.update({:title => title})
   redirect to('/projects')
 end
 
 delete('/projects/:id') do
-  project = Project.find(params[:id])
+  project = Project.find(params[:id].to_i)
   project.delete()
   redirect to('/projects')
+end
+
+get('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i)
+  erb(:volunteers)
+end
+
+post('/projects/:id/volunteers') do
+  name = params[:volunteer_name]
+  prj_id = params[:id].to_i
+  volunteer = Volunteer.new({:name => name, :project_id => prj_id, :id => nil})
+  volunteer.save()
+  @project = Project.find(params[:id].to_i)
+  erb(:volunteers) 
+end
+
+get('/projects/:id/volunteers/:v_id/edit') do
+  @project = Project.find(params[:id].to_i)
+  @volunteer = Volunteer.find(params[:v_id].to_i)
+  erb(:edit_volunteer)
+end
+
+patch('/projects/:id/volunteers/:v_id') do
+  name = params[:volunteer_name]
+  prj_id = params[:id].to_i
+  volunteer = Volunteer.find(params[:v_id].to_i)
+  volunteer.update(name, prj_id)
+  @project = Project.find(prj_id)
+  erb(:volunteers)
+end
+
+delete('/projects/:id/volunteers/:v_id') do
+  volunteer = Volunteer.find(params[:v_id].to_i)
+  volunteer.delete()
+  @project = Project.find(params[:id].to_i)
+  erb(:volunteers)
 end
